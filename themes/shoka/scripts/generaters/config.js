7ab3533c754a44a1d2c62354af8778867444cedb
Win7GM <Cc741852963@hotmail.com>
2020-09-28 13:35:13 +0800

    deletion

--- themes/shoka/scripts/generaters/config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const merge = require('hexo-util').deepMerge || require('lodash/merge');
-
-hexo.on('generateBefore', function () {
-  if (hexo.config.theme_config) {
-    hexo.theme.config = merge(hexo.theme.config, hexo.config.theme_config);
-  }
-})
